a9b67d33272269062e3b220200fb9c5b2e5f6231
Emre Özdemir <emreozdemir@Emres-Laptop.local>
2024-11-05 15:42:53 +0300

    first commit

--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,16 @@
-import scapy
+from scapy.all import IP, ICMP, send
 
-# Implement your ICMP sender here
+def send_icmp_packet():
+    target_ip = "172.18.0.3"
+    
+    ip_packet = IP(dst=target_ip, ttl=1)
+    
+    icmp_packet = ICMP(type="echo-request")
+    
+    packet = ip_packet / icmp_packet
+    
+    send(packet)
+    print("ICMP request packet sent with TTL=1")
+
+if __name__ == "__main__":
+    send_icmp_packet()
